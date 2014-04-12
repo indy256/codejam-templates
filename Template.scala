c9890e7c3f9eff7789462b5f961a45116d3f6bba
Andrey Naumenko <indy256@gmail.com>
2014-04-12 11:42:52 +0300

    update

--- Template.scala
+++ Template.scala
@@ -1,8 +1,5 @@
-import java.io.File;
-import java.io.PrintWriter;
-import java.util.Arrays;
-import java.util.Locale;
-import java.util.Scanner;
+import java.io._;
+import java.util._;
 
 object Template {
 
@@ -14,33 +11,23 @@ object Template {
 
     val sc = new Scanner(new File(path + name + ".in"))
     val pw = new PrintWriter(path + name + ".out")
-
+    
 //    val sc = new Scanner(System.in)
 //    val pw = new PrintWriter(System.out)
 
-    val testCases = sc.nextInt()
+    val testCases = sc.nextInt
 
     for (testCase <- 1 to testCases) {
-      val cmd = sc.next()
-      val n : Int = sc.nextInt()
-      var a = new Array[Double](n)
-      for (i <- 0 to n - 1) {
-        a(i) = sc.nextDouble()
+      val cmd = sc.next
+      val n : Int = sc.nextInt
+      val a = Seq.fill(n)(sc.nextDouble)
+
+      val res = cmd match {
+        case "median" => a.sorted.apply(n / 2)
+        case "mean" => a.sum / n
       }
 
-      var res = 0.0
-      if (cmd == "median") {
-        Arrays.sort(a)
-        res = a(n / 2)
-      } else if (cmd == "mean") {
-        var sum = 0.0
-        for (i <- 0 to n - 1) {
-          sum += a(i)
-        }
-        res = sum / n
-      }
-
-      pw.println("Case #" + testCase + ": %.10f".format(res))
+      pw.println(s"Case #${testCase}" + ": %.10f".format(res))
       pw.flush()
     }
 
