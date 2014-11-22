5a40931080e229d5dbda4a235c436828090cea18
Andrey Naumenko <indy256@gmail.com>
2014-11-23 00:10:14 +0300

    update

--- template-node.js
+++ template-node.js
@@ -1,35 +1,35 @@
 // node template-node.js < A-small.in > A-small.out
 
 function main() {
-  var testCases = readline();
+    var testCases = readline();
 
-  for (var testCase = 1; testCase <= testCases; ++testCase) {
-      var line = readline().split(' ');
-      var cmd = line[0];
-      var n = parseInt(line[1]);
-      var a = [];
-      for (var i = 0; i < n; i++) {
-          a[i] = parseFloat(line[i + 2]);
-      }
+    for (var testCase = 1; testCase <= testCases; ++testCase) {
+        var line = readline().split(' ');
+        var cmd = line[0];
+        var n = parseInt(line[1]);
+        var a = [];
+        for (var i = 0; i < n; i++) {
+            a[i] = parseFloat(line[i + 2]);
+        }
 
-      var res;
+        var res;
 
-      switch (cmd) {
-          case "median":
-              a.sort();
-              res = a[n >> 1];
-              break;
-          case "mean":
-              var sum = 0;
-              for (var j = 0; j < n; j++) {
-                  sum += a[j];
-              }
-              res = sum / n;
-              break;
-      }
+        switch (cmd) {
+            case "median":
+                a.sort();
+                res = a[n >> 1];
+                break;
+            case "mean":
+                var sum = 0;
+                for (var j = 0; j < n; j++) {
+                    sum += a[j];
+                }
+                res = sum / n;
+                break;
+        }
 
-      print('Case #' + testCase + ': ' + res.toFixed(10))
-  }
+        print('Case #' + testCase + ': ' + res.toFixed(10))
+    }
 }
 
 // auxiliary code
