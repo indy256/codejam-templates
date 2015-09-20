4aa2d090b54d610518715ad766583217a95e2801
indy256 <indy256@gmail.com>
2015-09-21 00:43:16 +0300

    update

--- template.hs
+++ template.hs
@@ -6,10 +6,11 @@ main = interact $
   unlines . zipWith output [1..] . map (solve.words) . tail . lines
 
 output :: Int -> Double -> String
-output testCase res = "Case #" ++ show testCase ++ ": " ++ printf "%.10f" res
+output testCase res = printf "Case #%d: %.10f" testCase res
 
 solve :: [String] -> Double
 solve (cmd: x)
-  | cmd == "mean" = (foldl (+) 0 a) / (fromIntegral $ ((length x) - 1))
-  | cmd == "median" = sort a !! (quot ((length x) - 1) 2)
-  where a = map read $ tail x
+  | cmd == "mean" = sum a / (fromIntegral $ length list)
+  | cmd == "median" = sort a !! (length list `div` 2)
+  where a = map read $ list
+        list = tail x
