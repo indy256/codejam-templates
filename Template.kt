ab927808413d7a646874d41cba7e3fecea05ff36
Subash Poudyal <subash.poudyal8@gmail.com>
2020-04-12 16:09:34 +0100

    add Kotlin template (#4)

---
+++ Template.kt
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+
+fun main(args: Array<String>) {
+    val name = "A-small"
+    val path = ""
+
+    Locale.setDefault(Locale.US)
+
+    val sc = Scanner(File("$path$name.in"))
+    val pw = PrintWriter("$path$name.out")
+
+    val testCases = sc.nextInt()
+
+    for(testCase in 1..testCases) {
+        val cmd = sc.next()
+        val n = sc.nextInt()
+        val a = DoubleArray(n) { sc.nextDouble() }
+
+        val result = when (cmd) {
+            "median" -> a.sortedArray()[n / 2]
+            "mean" -> a.sum() / n
+            else -> throw RuntimeException("Command not valid")
+        }
+        pw.printf("Case #$testCase: %.10f\n", result)
+        pw.flush()
+    }
+
+    pw.close()
+    sc.close()
+}
